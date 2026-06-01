aae9477f2f9ac7311878879da30a58aabbe54abb
Taras <19Zefir@gmail.com>
2026-06-01 15:30:52 +0300

    fix img

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/wedding/i11.jpg",
+        url: "/wedding/i12.jpg",
         width: 1200,
         height: 630,
       },
